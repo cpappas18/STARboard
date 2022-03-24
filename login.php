acc7fac3aac52e27bdf7499d8e810a4b6f81a792
Chloe Pappas <chloeoliviapappas@gmail.com>
2022-03-24 17:08:56 -0400

    Redirection bug fixed

--- login.php
+++ login.php
@@ -1,20 +1,14 @@
-<html>
-    <head>
-    </head>
+<?php 
+// TODO: implement login verification
+$login_success = true;
 
-    <body>
-        <!-- TODO: remove all this code, implement login verification -->
-
-        Welcome <?php echo $_POST["username"];?><br>
-        Your encrypted password is <?php echo $_POST["password"];?><br>
-
-        <?php 
-            $login_success = true;
-
-            // TODO: redirection is not working for some reason
-            if ($login_success) {
-                echo '<script> window.location.replace("dashboard.html"); </script>';
-            }
-        ?>
-    </body>
-</html>
+if ($login_success) {
+    echo 
+    '<script> 
+        function redirect() { 
+            console.log("redirecting");
+            window.location.replace("dashboard.html"); 
+        } 
+    </script>';
+}
+?>
