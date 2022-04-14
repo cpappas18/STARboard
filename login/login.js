b6342f9ab6753cb01d812cd3e3686b2d74d17e5d
cpappas18 <chloeoliviapappas@gmail.com>
2022-04-14 01:48:12 -0400

    Added user input confirmation to the registration form

--- login/login.js
+++ login/login.js
@@ -29,29 +29,98 @@ function toggleLogin() {
     }
 }
 
-function sendAccountCreationRequest() {
-    let username = document.getElementById("username-reg").value;
-    let password = document.getElementById("password-reg").value;
-    let first_name = document.getElementById("first-name").value;
-    let last_name = document.getElementById("last-name").value;
-    let email = document.getElementById("email").value;
-    let student_id = document.getElementById("student-id").value;
-
-    // get account types from form input
-    let checked = document.querySelectorAll('input[name=acct-type]:checked');
-    let account_types = [];
-    checked.forEach(type => account_types.push(type.id));
-
-    // get registered courses from form input
+/**
+ * Returns registered courses from form input
+ */
+function getRegisteredCourses() {
     let courses = document.getElementById("courses");
     let reg_courses = [];
     let course;
+
     for (let c=0; c<courses.children.length; c++) {
         course = courses.children[c].children[0]; // access nested input element
         if (course.value.length > 0) {
             reg_courses.push(course.value);
         }
     }
+
+    return reg_courses;
+}
+
+/**
+ * Returns account types from form input
+ */
+function getAccountTypes() {
+    let checked = document.querySelectorAll('input[name=acct-type]:checked');
+    let account_types = [];
+    checked.forEach(type => account_types.push(type.id));
+    return account_types;
+}
+
+/**
+ * Clear all error messages
+ */
+function clearErrorMessages() {
+    const error_div = document.getElementById("error-msg-cont");
+
+    while (error_div.firstChild) {
+        error_div.removeChild(error_div.lastChild);
+    }
+}
+
+
+function confirmFieldsCompleted() {
+    let username = document.getElementById("username-reg").value;
+    let password = document.getElementById("password-reg").value;
+    let first_name = document.getElementById("first-name").value;
+    let last_name = document.getElementById("last-name").value;
+    let email = document.getElementById("email").value;
+    let account_types = getAccountTypes();
+    let student_id = document.getElementById("student-id").value;
+    let reg_courses = getRegisteredCourses();
+
+    if (account_types.includes("student") || account_types.includes("TA")) {
+        // students and TAs must have a student ID
+        if (student_id.length == 0) {
+            return false;
+        }
+
+        // students must also have registered courses
+        if (account_types.includes("student")) {
+            if (reg_courses.length == 0) {
+                return false;
+            }
+        }
+    }
+
+    // all other fields must be filled out
+    if (username.length > 0 && password.length > 0 && first_name.length > 0 
+        && last_name.length > 0 && email.length > 0 && account_types.length > 0) {
+            return true;
+    } else {
+        return false;
+    }
+        
+}
+
+function sendAccountCreationRequest() {
+    clearErrorMessages();
+    let complete = confirmFieldsCompleted();
+
+    if (!complete) {
+        let error_div = document.getElementById("error-msg-cont");
+        error_div.innerHTML = "<p style='color:red'>All fields are required.</p>";
+        return;
+    }
+
+    let username = document.getElementById("username-reg").value;
+    let password = document.getElementById("password-reg").value;
+    let first_name = document.getElementById("first-name").value;
+    let last_name = document.getElementById("last-name").value;
+    let email = document.getElementById("email").value;
+    let student_id = document.getElementById("student-id").value;
+    let account_types = getAccountTypes(); // get account types from form input
+    let reg_courses = getRegisteredCourses(); // get registered courses from form input
 
     try {
         const syncRequest = new XMLHttpRequest();
@@ -62,9 +131,25 @@ function sendAccountCreationRequest() {
         &studentid=${student_id}&courses=${JSON.stringify(reg_courses)}`);
 
         if (syncRequest.status === 200) {
-            document.getElementById("reg-form").innerHTML = syncRequest.responseText;
+            let parser = new DOMParser();
+            let xmlDoc = parser.parseFromString(syncRequest.responseText, "text/xml");
+            let error_msgs = xmlDoc.getElementsByClassName("error");
+            
+            // check if we received an error while trying to register
+            if (error_msgs.length > 0) {
+                let error_div = document.getElementById("error-msg-cont");
+
+                // append all error messages
+                for (msg of error_msgs) {
+                    error_div.appendChild(msg);
+                }
+            } 
+            else { // registration success
+                document.getElementById("reg-form").innerHTML = syncRequest.responseText;
+            }
         }
     } catch (exception) {
+        console.log(exception);
         alert("Request failed. Please try again.");
     }
 }
